eead153f92bd4e38427562cc62337a115976be1a
Gustavo Alexandre <gustavo.nunes@pontotel.com.br>
2022-09-08 09:57:13 -0300

    considerações iniciais

---
+++ tests/test_pytest.py
@@ -0,0 +1,27 @@
+#def test_meu_primeiro_teste():
+#    assert True
+
+from codigo.jogo import brincadeira
+
+"""
+O teste é formado por tres etapas (GWT - AAA):
+1- Given - Dado
+2- When - Quando
+3- Then - Então
+
+- Arange
+- Act
+- Assert
+"""
+
+def test_quando_brincadeira_receber_1_entao_deve_retornar_1():
+    """
+        Brincadeira - SUT - System Under Test
+    """
+    entrada = 1                        #Given
+    esperado = 1                       #Given
+    resultado = brincadeira(entrada)   #When
+    assert resultado == esperado       #Then
+
+    #Versao pequena
+    #assert brincadeira(1)==1
